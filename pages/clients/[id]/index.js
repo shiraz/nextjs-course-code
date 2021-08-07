3a0a0b0715c28a81bc065c09cfb7cc94b695278f
mirzasb <mirzasbaig@nyu.edu>
2021-08-06 21:22:17 -0400

    Section 3 complete

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -6,9 +6,17 @@ const ClientsProjectsPage = () => {
     console.log(router.pathname);
     console.log(router.query);
 
+    const loadProjectHandler = () => {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: { id: 'max', clientprojectid: 'projecta' }
+        });
+    };
+
     return (
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
